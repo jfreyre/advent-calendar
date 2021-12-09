9f5decfa8e7c34b8e94d6e4ebd237280fa8b9989
jfreyre <jerome.freyre@gmail.com>
2021-12-09 16:43:58 +0100

    ex7 - part2

--- exercises/day7-part1.js
+++ exercises/day7-part1.js
@@ -1,7 +1,26 @@
-import { mock as data } from '../data/day7.js';
+import { input as data } from '../data/day7.js';
+
+function calculateEffort(index) {
+  let effort = 0;
+  for (var i = 0; i < data.length; i++) {
+    let current = Math.abs(data[i] - index);
+
+    effort += (current * (current + 1)) / 2;
+  }
+  return effort;
+}
 
 function resolve() {
-  console.log(data);
+  let minEffort = 99999999999999999999999;
+
+  for (var i = 0; i < data.length; i++) {
+    let currentEffort = calculateEffort(i);
+
+    if (currentEffort < minEffort) {
+      console.log('new min effort on ', currentEffort);
+      minEffort = currentEffort;
+    }
+  }
 }
 
 resolve();
