4222d39b270ca021d3f403740d84c48a26b744a7
jfreyre <jerome.freyre@gmail.com>
2021-12-09 22:23:55 +0100

    3 files modified

--- exercises/day8-part1.js
+++ exercises/day8-part1.js
@@ -1,26 +1,19 @@
-import { mock as data } from '../data/day8.js';
+import { input as data } from '../data/day8.js';
 
-function calculateEffort(index) {
-  let effort = 0;
-  for (var i = 0; i < data.length; i++) {
-    let current = Math.abs(data[i] - index);
-
-    effort += (current * (current + 1)) / 2;
-  }
-  return effort;
+function decodeLine(line) {
+  return line.filter((word) => [2, 3, 4, 7].includes(word.length)).length;
 }
 
 function resolve() {
-  let minEffort = Number.MAX_SAFE_INTEGER;
+  let numberOfUnique = 0;
 
-  for (var i = 0; i < data.length; i++) {
-    let currentEffort = calculateEffort(i);
-
-    if (currentEffort < minEffort) {
-      console.log('new min effort on ', currentEffort);
-      minEffort = currentEffort;
-    }
+  for (let i = 0; i < data.length; i++) {
+    var output = data[i].split('|')[1].split(' ');
+    console.log(output);
+    numberOfUnique += decodeLine(output);
   }
+
+  console.log(numberOfUnique);
 }
 
 resolve();
