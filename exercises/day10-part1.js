cbd7282a4387a76f3f86691ce8e26b87d82b927e
jfreyre <jerome.freyre@gmail.com>
2021-12-10 11:49:18 +0100

    1 file modified

--- exercises/day10-part1.js
+++ exercises/day10-part1.js
@@ -1,18 +1,54 @@
 import { mock as data } from '../data/day10.js';
+let scores = {
+  ')': 3,
+  ']': 57,
+  '}': 1197,
+  '>': 25137,
+};
+let map = {
+  ')': '(',
+  '}': '{',
+  ']': '[',
+  '>': '<',
+};
 
-const OPENING_CHAR = ['(', '[', '{', '<'];
-const CLOSING_CHAR = [')', ']', '}', '>'];
+const OPENING = Object.values(map);
+const CLOSING = Object.keys(map);
 
-function analyseLine(line) {
-  let i = 0;
-  while (i < line.length) {
-    const current = line[i];
-    if (OPENING_CHAR.indexOf >= 0) {
+function isIncomplete(s) {
+  OPENING.map((c) => {
+    (s.match(`${c}`) || []).length;
+  });
+}
 
+function isValid(s) {
+  let stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (OPENING.indexOf(s[i]) >= 0) {
+      stack.push(s[i]);
+    } else if (stack[stack.length - i] === map[s[i]]) {
+      stack.pop();
+    } else {
+      return s[i];
     }
-    
-    i++;
   }
+
+  return stack.length ? stack : true;
+}
+function analyseLine(line) {
+  if (isIncomplete(line)) {
+    return 0;
+  }
+
+  let r = isValid(line);
+  if (r === true) {
+    console.log(line, 'is valid');
+  } else {
+    console.log(r, scores[r]);
+  }
+
+  return scores[r];
 }
 function resolve() {
   let score = 0;
