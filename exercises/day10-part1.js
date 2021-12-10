a1b90dc1051bf0fdfa2e45c0a5d0fb4a918e50a1
jfreyre <jerome.freyre@gmail.com>
2021-12-10 07:23:22 +0100

    2 files modified

--- exercises/day10-part1.js
+++ exercises/day10-part1.js
@@ -1,1 +1,27 @@
 import { mock as data } from '../data/day10.js';
+
+const OPENING_CHAR = ['(', '[', '{', '<'];
+const CLOSING_CHAR = [')', ']', '}', '>'];
+
+function analyseLine(line) {
+  let i = 0;
+  while (i < line.length) {
+    const current = line[i];
+    if (OPENING_CHAR.indexOf >= 0) {
+
+    }
+    
+    i++;
+  }
+}
+function resolve() {
+  let score = 0;
+  for (var i = 0; i < data.length; i++) {
+    const line = data[i];
+    score += analyseLine(line);
+  }
+
+  console.log('final score is ', score);
+}
+
+resolve();
