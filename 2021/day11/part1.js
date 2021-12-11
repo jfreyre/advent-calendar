c71ca36f00291497e469d64e275fefb362fd8cd0
jfreyre <jerome.freyre@gmail.com>
2021-12-11 08:35:26 +0100

    2 files modified

--- 2021/day11/part1.js
+++ 2021/day11/part1.js
@@ -1,12 +1,64 @@
-export const mock = [
-  [5, 4, 8, 3, 1, 4, 3, 2, 2, 3],
-  [2, 7, 4, 5, 8, 5, 4, 7, 1, 1],
-  [5, 2, 6, 4, 5, 5, 6, 1, 7, 3],
-  [6, 1, 4, 1, 3, 3, 6, 1, 4, 6],
-  [6, 3, 5, 7, 3, 8, 5, 4, 7, 8],
-  [4, 1, 6, 7, 5, 2, 4, 6, 4, 5],
-  [2, 1, 7, 6, 8, 4, 1, 7, 2, 1],
-  [6, 8, 8, 2, 8, 8, 1, 1, 3, 4],
-  [4, 8, 4, 6, 8, 4, 8, 5, 5, 4],
-  [5, 2, 8, 3, 7, 5, 1, 5, 2, 6],
-];
+import { mock as data } from './data.js';
+
+function tryToIncrement(i, j) {
+  try {
+    data[i][j] += 1;
+  } 
+}
+function flash(flashed, i, j) {
+  // Exit condition
+  if (flashed.indexOf(i + '' + j) >= 0) {
+    return;
+  }
+  
+  for(var updatedI = -1; updatedI <= 1; updatedI++) {
+    for(var updatedJ = -1; updatedJ <= 1; updatedJ++) {
+      try {
+        data[updatedI][updatedJ] += 1;
+        flashed.push(updatedI+''+updatedJ)
+        if (data[updatedI][updatedJ] >= 9) {
+          flashed(flashed, updatedI, updatedJ)
+        }
+      } finally{ 
+        
+      }
+    }
+  }
+
+}
+
+function step() {
+  // First we update everybody
+  for (var i = 0; i < data.length; i++) {
+    for (let j = 0; i < data[i].length; j++) {
+      data[i][j] += 1;
+    }
+  }
+
+  let flashed = [];
+  for (var i = 0; i < data.lengt; i++) {
+    for (let j = 0; i < data[i].length; j++) {
+      if (data[i][j] >= 9) {
+        flash(flashed, i, j);
+      }
+    }
+  }
+
+  // Then we clean
+  for (var i = 0; i < data.lengt; i++) {
+    for (let j = 0; i < data[i].length; j++) {
+      if (data[i][j] >= 9) {
+        data[i][j] = 0;
+      }
+    }
+  }
+}
+
+function solve() {
+  const iteration = 3;
+  for (let i = 0; i < iteration; i++) {
+    step();
+  }
+}
+
+solve();
