c0f11b126d7e8b4ed666205b7a3bda19fa3cca58
jfreyre <jerome.freyre@gmail.com>
2021-12-11 09:10:18 +0100

    1 file modified

--- 2021/day11/part1.js
+++ 2021/day11/part1.js
@@ -1,22 +1,29 @@
 import { mock as data } from './data.js';
 const MAX_LIGHT_VALUE = 9;
 let flashes = 0;
+
 function flash(flashed, i, j) {
   // Exit condition
-  if (flashed.indexOf(i + '' + j) >= 0) {
+  if (flashed.indexOf(i + '_' + j) >= 0) {
     return;
   }
-  console.log('flash for ', i, ' ', j);
-  flashes++;
 
-  for (var updatedI = -1; updatedI <= 1; updatedI++) {
-    for (var updatedJ = -1; updatedJ <= 1; updatedJ++) {
+  flashed.push(i + '_' + j);
+
+  console.log('flashing ', i, ' ', j);
+
+  for (var verticalIndex = -1; verticalIndex <= 1; verticalIndex++) {
+    for (var horizontalIndex = -1; horizontalIndex <= 1; horizontalIndex++) {
       try {
-        data[updatedI][updatedJ] += 1;
-        flashed.push(updatedI + '' + updatedJ);
-        if (data[updatedI][updatedJ] >= MAX_LIGHT_VALUE) {
-          flashed(flashed, updatedI, updatedJ);
+        if (verticalIndex == 0 && horizontalIndex == 0) {
+          continue;
         }
+        data[i + verticalIndex][j + horizontalIndex] += 1;
+
+        if (data[i + verticalIndex][j + horizontalIndex] > MAX_LIGHT_VALUE) {
+          flashed(flashed, i + verticalIndex, j + horizontalIndex);
+        }
+      } catch (e) {
       } finally {
       }
     }
@@ -24,6 +31,7 @@ function flash(flashed, i, j) {
 }
 
 function step() {
+  console.log('new step');
   // First we update everybody
   for (var i = 0; i < data.length; i++) {
     for (let j = 0; j < data[i].length; j++) {
@@ -32,28 +40,33 @@ function step() {
   }
 
   let flashed = [];
-  for (var i = 0; i < data.lengt; i++) {
+  for (var i = 0; i < data.length; i++) {
     for (let j = 0; j < data[i].length; j++) {
-      if (data[i][j] >= MAX_LIGHT_VALUE) {
+      if (data[i][j] > MAX_LIGHT_VALUE) {
         flash(flashed, i, j);
       }
     }
   }
 
+  console.log(flashed.length, flashed);
+
   // Then we clean
-  for (var i = 0; i < data.lengt; i++) {
+  for (var i = 0; i < data.length; i++) {
     for (let j = 0; j < data[i].length; j++) {
-      if (data[i][j] >= MAX_LIGHT_VALUE) {
+      if (data[i][j] > MAX_LIGHT_VALUE) {
         data[i][j] = 0;
       }
     }
   }
+
+  return flashed;
 }
 
 function solve() {
   const iteration = 10;
   for (let i = 0; i < iteration; i++) {
-    step();
+    let result = step();
+    flashes += result.length;
   }
 
   console.log(flashes);
