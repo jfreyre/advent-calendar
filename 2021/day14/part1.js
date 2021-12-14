c93533706cd90c393168b8ff0b71d18782d74574
jfreyre <jerome.freyre@gmail.com>
2021-12-14 07:19:24 +0100

    1 file modified

--- 2021/day14/part1.js
+++ 2021/day14/part1.js
@@ -1,5 +1,19 @@
 import { mock as data, mockPolymer as polymer } from './data.js';
 
-function solve() {}
+function solve() {
+  let output = '';
+  for (let i = 0; i < polymer.length - 1; i++) {
+    let sub = polymer.substring(i, i + 1);
+    console.log(sub);
+
+    let index = data.indexOf(sub);
+    if (index => 0) {
+      output += `${polymer[i]}${index}${polymer[i+1]}`
+    } else {
+      output += sub;
+    }
+  }
+  console.log(output)
+}
 
 solve();
