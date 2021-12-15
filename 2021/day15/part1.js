7591916661a1b65bbb2a4bd163385c81e8c65997
jfreyre <jerome.freyre@gmail.com>
2021-12-15 15:03:18 +0100

    1 file added

---
+++ 2021/day15/part1.js
@@ -0,0 +1,59 @@
+import { data as data } from './data.js';
+
+const travel = (array, from, to) => {
+  const go = (i, j, smallest) => {
+    // Current score is already to high
+    if (smallest >= currentSmallestEffort) {
+      return;
+    }
+
+    // We've reached the destination!
+    if (i == to[0] && j == to[1]) {
+      currentSmallestEffort = smallest;
+
+      // console.log('reached!', smallest);
+      return;
+    }
+
+    if (unvisited[i]?.[j] > smallest) {
+      unvisited[i][j] = smallest;
+      let value = smallest + +array[i][j];
+
+      go(i, j + 1, value);
+      go(i - 1, j, value);
+      go(i + 1, j, value);
+      go(i, j - 1, value);
+    }
+  };
+
+  let currentSmallestEffort = Number.MAX_VALUE;
+  // We build a matrix with hight cost
+  let unvisited = array.map((a) => a.split('').map((_) => Number.MAX_VALUE));
+
+  // debugger;
+  go(from[0], from[1], 0);
+  console.log(currentSmallestEffort);
+  return unvisited;
+};
+
+let testMatrix = ['01111', '12321', '12521', '12221'];
+
+testMatrix = data;
+debugger;
+const from = [0, 0];
+const to = [
+  testMatrix.length - 1,
+  testMatrix[testMatrix.length - 1].length - 1,
+];
+
+let result = travel(testMatrix, from, to);
+
+// 502 -> too high
+// 493 -> too slow
+// result.forEach((a) =>
+//   console.log(
+//     a
+//       .map((v) => (v === Number.MAX_VALUE ? '**' : ('0' + v).slice(-2)))
+//       .join(' ')
+//   )
+// );
