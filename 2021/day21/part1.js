4b06cbb34378e578c060e067209c0bf045826ee4
jfreyre <jerome.freyre@gmail.com>
2021-12-22 08:44:40 +0100

    1 file modified

--- 2021/day21/part1.js
+++ 2021/day21/part1.js
@@ -1,40 +1,40 @@
 const BOARD_GAME_SIZE = 10;
 
-function solve() {
-  let playersPosition = [4, 8];
-  let scores = [0, 0];
-  let dice = 0;
+let players = [
+  { position: 4, score: 0 },
+  { position: 8, score: 0 },
+];
 
-  let rollDice = () => {
-    let sum = ++dice + ++dice + ++dice;
-    if (dice === 99) {
-      dice = 0;
-    }
-    return sum;
-  };
+let dice = 0;
 
-  function playFor(playerIndex) {
-    const currentScore = rollDice();
-    playersPosition[playerIndex] += currentScore;
-
-    scores[playerIndex] +=
-      playersPosition[playerIndex] === BOARD_GAME_SIZE
-        ? BOARD_GAME_SIZE
-        : playersPosition[playerIndex] % BOARD_GAME_SIZE;
-
-    console.log(
-      playerIndex + 1,
-      dice,
-      currentScore,
-      playersPosition[playerIndex],
-      scores[playerIndex]
-    );
+let rollDice = () => {
+  let sum = ++dice + ++dice + ++dice;
+  if (dice === 99) {
+    dice = 0;
   }
+  return sum;
+};
 
-  do {
-    playFor(0);
-    playFor(1);
-  } while (!scores.some((s) => s >= 100));
+function playFor(playerIndex) {
+  const currentScore = rollDice();
+  players[playerIndex].position += currentScore;
+
+  players[playerIndex].score +=
+    players[playerIndex].position === BOARD_GAME_SIZE
+      ? BOARD_GAME_SIZE
+      : players[playerIndex].position % BOARD_GAME_SIZE;
+
+  console.log(
+    playerIndex + 1,
+    dice,
+    currentScore,
+    players[playerIndex].position,
+    players[playerIndex].score
+  );
 }
 
-solve();
+do {
+  for (let i = 0; i < players.length; i++) {
+    playFor(i);
+  }
+} while (!scores.some((s) => s >= 100));
