2baacbc3df5e5d2075550a567f794df8c03f7c07
jfreyre <jerome.freyre@gmail.com>
2021-12-22 09:31:42 +0100

    2 files modified

--- 2021/day21/part1.js
+++ 2021/day21/part1.js
@@ -1,5 +1,5 @@
 const BOARD_GAME_SIZE = 10;
-
+const WINNING_SCORE = 1_000;
 let nbTimesDiceRolled = 0;
 let dice = 0;
 // let players = [
@@ -26,58 +26,6 @@ function rollDice() {
 
 function playFor(playerIndex) {
   let player = players[playerIndex];
-  const BOARD_GAME_SIZE = 10;
-  const WINNING_SCORE = 1_000;
-
-  let nbTimesDiceRolled = 0;
-  let dice = 0;
-  // let players = [
-  //   { position: 4, score: 0 },
-  //   { position: 8, score: 0 },
-  // ];
-  let players = [
-    { position: 7, score: 0 },
-    { position: 2, score: 0 },
-  ];
-
-  let incDice = () => {
-    dice++;
-    nbTimesDiceRolled++;
-    dice = dice === 101 ? 1 : dice;
-    return dice;
-  };
-
-  function rollDice() {
-    let sum = incDice() + incDice() + incDice();
-
-    return sum;
-  }
-
-  function playFor(playerIndex) {
-    let player = players[playerIndex];
-    const currentScore = rollDice();
-    player.position += currentScore;
-
-    const mod = player.position % BOARD_GAME_SIZE;
-    player.score += mod === 0 ? 10 : mod;
-    // players[playerIndex].position === BOARD_GAME_SIZE
-    //   ? BOARD_GAME_SIZE
-    //   : players[playerIndex].position % BOARD_GAME_SIZE;
-
-    console.log(playerIndex + 1, dice, currentScore, players[playerIndex]);
-
-    if (player.score >= WINNING_SCORE) {
-      console.log(nbTimesDiceRolled);
-    }
-  }
-
-  do {
-    for (let i = 0; i < players.length; i++) {
-      playFor(i);
-    }
-  } while (!players.some((s) => s.score >= WINNING_SCORE));
-  console.log(nbTimesDiceRolled);
-
   const currentScore = rollDice();
   player.position += currentScore;
 
@@ -89,7 +37,7 @@ function playFor(playerIndex) {
 
   console.log(playerIndex + 1, dice, currentScore, players[playerIndex]);
 
-  if (player.score >= 1000) {
+  if (player.score >= WINNING_SCORE) {
     console.log(nbTimesDiceRolled);
   }
 }
@@ -98,5 +46,5 @@ do {
   for (let i = 0; i < players.length; i++) {
     playFor(i);
   }
-} while (!players.some((s) => s.score >= 1000));
+} while (!players.some((s) => s.score >= WINNING_SCORE));
 console.log(nbTimesDiceRolled);
