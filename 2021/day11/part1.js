9d92be5c6ed62cbb1bb78af811e15573e2d60c0e
jfreyre <jerome.freyre@gmail.com>
2021-12-22 11:30:13 +0100

    day 11  - part 1

--- 2021/day11/part1.js
+++ 2021/day11/part1.js
@@ -1,97 +1,54 @@
 import { mock as data } from './data.js';
 const MAX_LIGHT_VALUE = 9;
-let flashes = 0;
 
-function display() {
-  return;
-  for (var i = 0; i < data.length; i++) {
-    console.log(data[i]);
-  }
-}
+function step(state) {
+  let flashes = 0;
 
-let shouldFlash = (v) => v > MAX_LIGHT_VALUE;
+  function increment(x, y) {
+    // Are we out of bound ?
+    if (x < 0 || y < 0 || x > 9 || y > 9) {
+      return;
+    }
 
-function flash(flashed, i, j) {
-  const key = i + '_' + j;
+    state[x][y]++;
 
-  if (i < 0 || j < 0 || i > data.length - 1 || j > data[i].length - 1) {
-    return;
-  }
-
-  // Exit condition
-  if (flashed.indexOf(key) >= 0) {
-    return;
-  }
-
-  flashed.push(key);
-
-  for (var verticalIndex = -1; verticalIndex <= 1; verticalIndex++) {
-    for (var horizontalIndex = -1; horizontalIndex <= 1; horizontalIndex++) {
-      // We don't update ourselves
-      if (verticalIndex == 0 && horizontalIndex == 0) {
-        continue;
-      }
-
-      const vert = i + verticalIndex;
-      const hori = j + horizontalIndex;
-
-      // We don't care if we're out of range
-      let outOfRange =
-        vert < 0 ||
-        hori < 0 ||
-        vert > data.length - 1 ||
-        hori > data.length - 1;
-      if (outOfRange) {
-        continue;
-      }
-      data[vert][hori] += 1;
-
-      if (shouldFlash(data[vert][hori])) {
-        flash(flashed, vert, hori);
-      }
+    if (state[x][y] > MAX_LIGHT_VALUE) {
+      flashes++;
+      state[x][y] = -20;
+      increment(x - 1, y - 1);
+      increment(x - 1, y + 0);
+      increment(x - 1, y + 1);
+      increment(x + 0, y - 1);
+      increment(x + 0, y + 0);
+      increment(x + 0, y + 1);
+      increment(x + 1, y - 1);
+      increment(x + 1, y + 0);
+      increment(x + 1, y + 1);
     }
   }
-}
 
-function step() {
-  console.log('new step');
-  let flashed = [];
+  for (let x = 0; x < 10; x++) {
+    for (let y = 0; y < 10; y++) {
+      increment(x, y);
+    }
+  }
 
-  // First we update everybody
-  for (var i = 0; i < data.length; i++) {
-    for (let j = 0; j < data[i].length; j++) {
-      data[i][j] += 1;
-
-      if (shouldFlash(data[i][j])) {
-        flash(flashed, i, j);
+  for (let x = 0; x < 10; x++) {
+    for (let y = 0; y < 10; y++) {
+      if (state[x][y] < 0) {
+        state[x][y] = 0;
       }
     }
   }
 
-  console.log(flashed.length, flashed);
-
-  // Then we clean
-  for (let i = 0; i < flashed.length; i++) {
-    const coords = flashed[i].split('_');
-    data[coords[0]][coords[1]] = 0;
-  }
-
-  display();
-
-  return flashed;
+  return flashes;
 }
 
-function solve() {
-  const iteration = 100;
-  for (let i = 0; i < iteration; i++) {
-    let result = step();
-    flashes += result.length;
-  }
+const grid = data;
+let answer = 0;
 
-  console.log(flashes);
+for (let i = 0; i < 100; i++) {
+  answer += step(grid);
 }
 
-// not 1656 (too high)
-// not 1971 (too high)
-
-solve();
+console.log(`Answer 1: ${answer}`);
